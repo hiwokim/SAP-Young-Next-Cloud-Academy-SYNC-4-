a73b92437d747e0eef37d8334c80aa7a0d5b9ede
hiwokim <bio0407@gmail.com>
2024-01-19 18:22:56 +0900

    2024-01-19

--- project_10_13/webapp/controller/Main.controller.js
+++ project_10_13/webapp/controller/Main.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, Filter, Fragment) {
         "use strict";
         
         return Controller.extend("opdata.project1013.controller.Main", {
@@ -20,7 +22,9 @@ sap.ui.define([
                 this.getView().setModel(new JSONModel(oData), 'data');
             },
 
-            onRowSelectionChange: function(oEvent) {                    
+            onRowSelectionChange: function(oEvent) {  
+                if(!oEvent.getParameter('rowContext')) return;
+
                 var sPath = oEvent.getParameter('rowContext').getPath(); //Product('1000')                                
                 var oSelectData = this.getView().getModel().getProperty(sPath); //한 건의 model data - 모델경로로 해당 경로의 전체 데이터 얻음
 
@@ -62,21 +66,44 @@ sap.ui.define([
                 this.getView().getModel().refresh(true);
                
             },
-            onEntitySet: function(){
+            onEntitySet: function(oEvent){
                 // 전체 조회 구현
                 // GET 요청 : "/Products"
                 var oDataModel = this.getView().getModel();
+                var oFilter = new Filter("Productname","EQ","안녕");
+                var oDialog = this.getView().byId("idDialog");
 
-                oDataModel.read("/Products", {
-                    filters: [ /* 필터 객체 배열 */ ],
-                    success: function(oReturn){
-                        console.log("전체조회", oReturn);
-                    },
-                    error: function(oError){
-                        console.log("전체조회 중 오류 발생 ", oError);
-                    }
+                var oButton = oEvent.getSource(),
+				    oView = this.getView();
+
+                // create popover
+                if (!this._pPopover) {
+                    this._pPopover = Fragment.load({
+                        id: oView.getId(),
+                        name: "opdata.project1013.view.fragment.Popover",
+                        controller: this
+                    }).then(function(oPopover) {
+                        oPopover.setModel(new JSONModel(), 'popover')
+                        oView.addDependent(oPopover);
+                        return oPopover;
+                    });
+                }
+                this._pPopover.then(function(oPopover) {
+                    oPopover.openBy(oButton);
                 });
 
+                // oDataModel.read("/Products", {
+                //     filters: [oFilter],
+                //     success: function(oReturn){
+                //         console.log("전체조회", oReturn);
+
+                //         oDialog.setModel(new JSONModel(oReturn), "fragmentData");
+                //         oDialog.open();
+                //     },
+                //     error: function(oError){
+                //         console.log("전체조회 중 오류 발생 ", oError);
+                //     }
+                // });
             },
             onEntity: function(){
                 // 데이터 한 건 조회
@@ -159,6 +186,30 @@ sap.ui.define([
                     }
                 });
                 
+            },
+            onClose: function(oEvent) {
+                oEvent.getSource().getParent().close();
+            },
+            onRead: function(){
+                // var oPopover = sap.ui.getCore().byId("myPopover");
+
+                // Fragment.load() 사용
+                // view id를 같이 넘겨줬기 때문에 view 안에 popover가 붙게됨
+                // 따라서 this.byId() 로 접근 가능
+                var oPopover = this.byId("myPopover");
+                var oPopoverModel = oPopover.getModel('popover');
+                var oDataModel = this.getView().getModel();
+
+                var oFilter = new Filter("Productname","EQ",oPopoverModel.getData().Productname);
+                oDataModel.read("/Products", {
+                    filters: [oFilter],
+                    success: function(oReturn){
+                        console.log("전체조회", oReturn);
+                    },
+                    error: function(oError){
+                        console.log("전체조회 중 오류 발생 ", oError);
+                    }
+                });
             }
         });
     });
--- project_10_14/webapp/controller/Main.controller.js
+++ project_10_14/webapp/controller/Main.controller.js
@@ -2,12 +2,14 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageBox",
-	"sap/m/MessageToast"
+	"sap/m/MessageToast",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, MessageBox, MessageToast) {
+    function (Controller, JSONModel, MessageBox, MessageToast, Fragment, Filter) {
         "use strict";
         
         return Controller.extend("project1014.controller.Main", {
@@ -24,9 +26,9 @@ sap.ui.define([
             onRowSelectionChange: function(oEvent) {   
                 // Row 선택 해제 되었을 때도, '선택'된 것이기 떄문에 이벤트 발생
                 // 따라서 rowContext가 없을 땐 함수 종료하도록 함
-                if(!oEvent.getParameter('rowContext')) return;
+                if(!oEvent.getParameter('rowContext')) return; // 질문
                 
-                var sPath = oEvent.getParameter('rowContext').getPath();                               
+                var sPath = oEvent.getParameter('rowContext').getPath();                             
                 var oSelectData = this.getView().getModel().getProperty(sPath); 
 
                 //data 모델 가져와서
@@ -59,18 +61,36 @@ sap.ui.define([
                
             },
             // 전체 조회 : GET 요청
-            onEntitySet: function(){
+            onEntitySet: function(oEvent){
                 var oDataModel = this.getView().getModel();
+                var oButton = oEvent.getSource(),
+				    oView = this.getView();
 
-                oDataModel.read("/Member", {
-                    success: function(oReturn){
-                        this._showMessage("전체조회 성공 ");
-                        console.log(oReturn);
-                    }.bind(this),
-                    error: function(){
-                        this._showErrorMessage("전체조회 중 오류 발생 ");
-                    }.bind(this)
+                // create popover
+                if (!this._pPopover) {
+                    this._pPopover = Fragment.load({
+                        id: oView.getId(),
+                        name: "project1014.view.fragment.Popover",
+                        controller: this
+                    }).then(function(oPopover) {
+                        oPopover.setModel(new JSONModel(), 'popover')
+                        oView.addDependent(oPopover);
+                        return oPopover;
+                    });
+                }
+                this._pPopover.then(function(oPopover) {
+                    oPopover.openBy(oButton);
                 });
+
+                // oDataModel.read("/Member", {
+                //     success: function(oReturn){
+                //         this._showMessage("N","전체조회 성공 ");
+                //         console.log(oReturn);
+                //     }.bind(this),
+                //     error: function(){
+                //         this._showMessage("E","전체조회 중 오류 발생 ");
+                //     }.bind(this)
+                // });
 
             },
             // 한 건 조회 : GET 요청
@@ -78,16 +98,16 @@ sap.ui.define([
                 var oDataModel = this.getView().getModel();
                 var oJSONData = this.getView().getModel('data').getData();
                 var sPath = oDataModel.createKey("/Member", {
-                    Memid : oJSONData.Memid
+                    Memid : oJSONData.Memid // Member('00001')
                 });
 
                 oDataModel.read(sPath, {
                     success: function(oReturn){
-                        this._showMessage("한건 조회 성공");
+                        this._showMessage("N","한건 조회 성공");
                         console.log(oReturn);
                     }.bind(this),
                     error: function(){
-                        this._showErrorMessage("한건 조회 중 오류 발생 ");
+                        this._showMessage("E","한건 조회 중 오류 발생 ");
                     }.bind(this)
                 });
             },
@@ -100,6 +120,10 @@ sap.ui.define([
                     Memnm : oJSONData.Memnm || "",
                     Telno : oJSONData.Telno || "",
                     Email : oJSONData.Email || ""
+                    
+                    // WorkSet : [
+                    //     {}, {}, {}, ...
+                    // ]
                 };
 
                 oDataModel.create("/Member", oBody, {
@@ -108,10 +132,10 @@ sap.ui.define([
                         // 해당 함수 안에서 this가 달라지기 때문에,
                         // 이전에 사용하던 this를 그대로 넘겨주기 위해서
                         // .bind(this)를 적용시킴
-                        this._showMessage("데이터 생성 완료");
+                        this._showMessage("N","데이터 생성 완료");
                     }.bind(this),
                     error: function(){
-                        this._showErrorMessage("에러 발생");
+                        this._showMessage("E","에러 발생");
                     }.bind(this)
                 });
             },
@@ -125,10 +149,10 @@ sap.ui.define([
 
                 oDataModel.update(sPath, oBody, {
                     success: function(){
-                        this._showMessage("데이터 업데이트 완료");
+                        this._showMessage("N","데이터 업데이트 완료");
                     }.bind(this),
                     error: function(){
-                        this._showErrorMessage("데이터 업데이트 실패");
+                        this._showMessage("E","데이터 업데이트 실패");
                     }.bind(this)
                 });
             },
@@ -143,20 +167,52 @@ sap.ui.define([
 
                 oDataModel.remove(sPath, {
                     success: function(){
-                        this._showSuccessMessage("삭제 완료");
+                        this._showMessage("S","삭제 완료");
                     }.bind(this)
                 });
                 
             },
 
-            _showMessage: function(msg) {
-                sap.m.MessageToast.show(msg);
+            _showMessage: function(ch, msg) {
+                switch(ch){
+                    case 'N':
+                        sap.m.MessageToast.show(msg);
+                        break;
+                    case 'S':
+                        sap.m.MessageBox.success(msg);
+                        break;
+                    case 'E':
+                        sap.m.MessageBox.error(msg);
+                        break;
+                }
             },
-            _showSuccessMessage: function(msg) {
-                MessageBox.success(msg);
-            },
-            _showErrorMessage: function(msg) {
-                MessageBox.error(msg);
-            },
+            onRead: function(){
+                // var oPopover = sap.ui.getCore().byId("myPopover");
+
+                // Fragment.load() 사용
+                // view id를 같이 넘겨줬기 때문에 view 안에 popover가 붙게됨
+                // 따라서 this.byId() 로 접근 가능
+                var oPopover = this.byId("myPopover");
+                var oPopoverModel = oPopover.getModel('popover');
+                var oData = oPopoverModel.getData();
+                var oDataModel = this.getView().getModel();
+
+                var oFilter = new Filter("Memnm","Contatins",oData);
+                oDataModel.read("/Member", {
+                    urlParameters: {
+                        "$expand" : "WorkSet",
+                        "$select" : "Memid,WorkSet" //workset만 조회가능
+                    },
+                    filters: [oFilter],
+                    success: function(oReturn){
+                        debugger;
+                        console.log(`검색어(${oData.Membername}) : `, oReturn.results);
+                    },
+                    error: function(oError){
+                        console.log("전체조회 중 오류 발생 ", oError);
+                    }
+                });
+            }
+            
         });
     });
